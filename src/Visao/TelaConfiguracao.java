faa743fe9bcd51e46e3fc8ed239fa0aa56d8b559
rodri <rodri@DESKTOP-36BQCL0>
2019-12-04 23:12:32 -0200

    fiz correçao de alguns bugs quando nao encontrava pessoal e evento na tala de inscriçao

--- src/Visao/TelaConfiguracao.java
+++ src/Visao/TelaConfiguracao.java
@@ -223,10 +223,7 @@ public class TelaConfiguracao extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
-        
-        try {
-            
-            this.dispose();
+                try {
             Xml.GeraArquivoConexao(jTextFieldServidor.getText(),Integer.valueOf(jTextFieldPorta.getText()), jTextFieldNomeBD.getText(), jTextFieldUsuarioBD.getText(), jTextFieldSenhaBD.getText());
         } catch (TransformerConfigurationException ex) {
             Logger.getLogger(TelaConfiguracao.class.getName()).log(Level.SEVERE, null, ex);
